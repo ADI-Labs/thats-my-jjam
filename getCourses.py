5761a2289e0531d4b69bef843c665735efde782d
Melanie Sawyer <melaniensawyer@gmail.com>
2016-10-14 16:52:43 -0400

    get culpa info

---
+++ getCourses.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+@app.route('/',methods = ['GET'])
+def getInfo():
+    r = requests.get('http://api.culpa.info/courses/department_id/7')
+    return r.text
+if __name__ == '__main__':
+        app.debug = True
+        app.run()
