552861765802c9685d5dffa4f4001484e675c0d3
Melanie Sawyer <melaniensawyer@gmail.com>
2016-10-20 18:33:12 -0400

    add updated course file

--- getCourses.py
+++ getCourses.py
@@ -1,10 +1,25 @@
 from flask import Flask, request
 import requests
+import json
 app = Flask(__name__)
 @app.route('/',methods = ['GET'])
+def getNames():
+    r = requests.get('http://api.culpa.info/courses/department_id/7')
+    jason_data = json.loads(r.text)
+    array = ""
+    for i in range(len(jason_data['courses'])):
+        array = array + str(jason_data['courses'][i]['name'])+'x'
+    return array
+
 def getInfo():
     r = requests.get('http://api.culpa.info/courses/department_id/7')
-    return r.text
+    jason_data = json.loads(r.text)
+    string = ""
+    for i in range(len(jason_data['courses'])):
+        print(string)
+        string += str(jason_data['courses'][i]['number'])+" "
+    data = jason_data['courses'][0]['number']
+    return string
 if __name__ == '__main__':
         app.debug = True
         app.run()
