c95d04a8f8d9c4d6ccdb6921c40a0e3004c04cd4
Alan Du <alanhdu@gmail.com>
2016-10-27 11:31:50 +0100

    Fix Flake8

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask
+import requests
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def getInfo():
+    r = requests.get('http://api.culpa.info/courses/department_id/7')
+    return r.text
+
+
+if __name__ == '__main__':
+    app.run()
